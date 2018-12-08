94a47403d3194a9c6292f81749d9e2397b99d58b
Vang Xiong <vxiong029@gmail.com>
2018-12-08 10:05:54 -0600

    add form to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+// Redux imports
+import { connect } from 'react-redux';
 
 class App extends Component {
+  state = {
+    rating: ''
+  }
+  handleChange = (event) => {
+    this.setState({
+      rating: event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -10,10 +20,33 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
-        <br/>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            How are you feeling today?
+            <input type="text" onChange={this.handleChange} name="feeling" />
+          </label>
+          <label>
+            How well are you understanding the content?
+            <input type="text" onChange={this.handleChange} name="understand" />
+          </label>
+          <label>
+            How well are you being supported?
+            <input type="text" onChange={this.handleChange} name="support" />
+          </label>
+          <label>
+            Any comments you want to leave?
+            <textarea type="text" onChange={this.handleChange} name="comments"></textarea>
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        <br />
       </div>
     );
   }
 }
+// allows us to access REDUX information as props
+const mapStateToProps = reduxStore => ({
 
-export default App;
+})
+export default connect()(App);
