8d0224e06fec489c93f962655ca7fffc2c78ceb7
Vang Xiong <vxiong029@gmail.com>
2018-12-08 11:19:58 -0600

    Feelings.js add function

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
 class Feeling extends Component {
+  setFeeling = () => {
+    this.props.dispatch({
+      type: 'SET_FEELING',
+      payload: this.state
+    });
+  }
   render() {
     return (
-
+      <div>
+        <label>
+            How are you feeling today?
+            <input type="text" onChange={this.handleChange} name="feeling" />
+            <button onClick={this.setFeeling}>Next</button>
+          </label>
+      </div>
     )
   }
 }
